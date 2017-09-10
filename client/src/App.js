eb2e5ad27ac8a241e23207d5cbb733cca779fef1
MikeNewm32 <michaelnewman85@hotmail.com>
2017-09-10 13:30:22 -0400

    init push

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          <h1>Hello World!!</h1>
+        </p>
+        </div>
+    );
+  }
+}
+
+export default App;
